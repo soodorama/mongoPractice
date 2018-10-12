d069f1a7824de2f8aa7ef2b74307ca5efbd4eece
Neil Sood <soodorama@gmail.com>
2018-10-12 14:44:18 -0700

    working mongoose server

--- basic_mongoose_app/server.js
+++ basic_mongoose_app/server.js
@@ -30,16 +30,42 @@ app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 // Routes
 // Root Request
+// The root route -- we want to get all of the users from the database and then render the index view passing it all of the users
 app.get('/', function(req, res) {
-    // This is where we will retrieve the users from the database and include them in the view page we will be rendering.
-    res.render('index');
-})
+    User.find({}, function(err, users) {
+        if (err) {
+            console.log("ERROR")
+        }
+        else {
+            console.log("SUCCESS")
+        }
+        res.render("index", {info: users})
+        // This is the method that finds all of the users from the database
+        // Notice how the first parameter is the options for what to find and the second is the
+        //   callback function that has an error (if any) and all of the users
+        // Keep in mind that everything you want to do AFTER you get the users from the database must
+        //   happen inside of this callback for it to be synchronous 
+        // Make sure you handle the case when there is an error, as well as the case when there is no error
+    })
+  })
+  
 // Add User Request 
 app.post('/users', function(req, res) {
     console.log("POST DATA", req.body);
-    // This is where we would add the user from req.body to the database.
-    res.redirect('/');
-})
+    // create a new User with the name and age corresponding to those from req.body
+    var user = new User({name: req.body.name, age: req.body.age});
+    // Try to save that new user to the database (this is the method that actually inserts into the db) and run a callback function with an error (if any) from the operation.
+    user.save(function(err) {
+      // if there is an error console.log that something went wrong!
+      if(err) {
+        console.log('something went wrong');
+      } else { // else console.log that we did well and then redirect to the root route
+        console.log('successfully added a user!');
+        res.redirect('/');
+      }
+    })
+  })
+  
 // Setting our Server to Listen on Port: 8000
 app.listen(8000, function() {
     console.log("listening on port 8000");
