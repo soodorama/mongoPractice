19178e0814b0e9ee0f0fa66ce4c5302056b4108a
Neil Sood <soodorama@gmail.com>
2018-10-13 16:51:33 -0700

    quoting dojo starting:

---
+++ quoting_dojo/server.js
@@ -0,0 +1,61 @@
+// Require the Express Module
+var express = require('express');
+// Create an Express App
+var app = express();
+// Require body-parser (to receive post data from clients)
+var bodyParser = require('body-parser');
+// Integrate body-parser with our App
+app.use(bodyParser.urlencoded({ extended: true }));
+// Require path
+var path = require('path');
+// Setting our Static Folder Directory
+app.use(express.static(path.join(__dirname, './static')));
+// Setting our Views Folder Directory
+
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/quoting_dojo");
+var UserSchema = new mongoose.Schema({
+    name: String,
+    quote: String,
+    date: Date
+})
+mongoose.model('Quote', UserSchema); // We are setting this Schema in our Models as 'User'
+var User = mongoose.model('Quote') // We are retrieving this Schema from our Models, named 'User'
+   
+
+
+app.set('views', path.join(__dirname, './views'));
+// Setting our View Engine set to EJS
+app.set('view engine', 'ejs');
+// Routes
+// Root Request
+app.get('/', function(req, res) {
+    User.find({}, function(err, users) {
+        // This is the method that finds all of the users from the database
+        // Notice how the first parameter is the options for what to find and the second is the
+        //   callback function that has an error (if any) and all of the users
+        // Keep in mind that everything you want to do AFTER you get the users from the database must
+        //   happen inside of this callback for it to be synchronous 
+        // Make sure you handle the case when there is an error, as well as the case when there is no error
+      })
+    // This is where we will retrieve the users from the database and include them in the view page we will be rendering.
+    res.render('index');
+})
+// Add User Request 
+app.post('/quotes', function(req, res) {
+    var quote = new Quote({name: req.body.name, quote: req.body.quote, date: date.now()});
+    // Try to save that new user to the database (this is the method that actually inserts into the db) and run a callback function with an error (if any) from the operation.
+    quote.save(function(err) {
+        // if there is an error console.log that something went wrong!
+        if(err) {
+            console.log('something went wrong');
+        } else { // else console.log that we did well and then redirect to the root route
+            console.log('successfully added a user!');
+            res.render('quotes');
+        }
+    })
+})
+// Setting our Server to Listen on Port: 8000
+app.listen(8000, function() {
+    console.log("listening on port 8000");
+})
