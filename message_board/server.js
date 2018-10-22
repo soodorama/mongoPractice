61900d765e102bab696296b19356a00d4f96fce2
Neil Sood <soodorama@gmail.com>
2018-10-22 16:12:10 -0700

    add working without text

--- message_board/server.js
+++ message_board/server.js
@@ -6,6 +6,22 @@ var path = require('path');
 app.use(express.static(path.join(__dirname, './static')));
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/message_board");
+
+const CommentSchema = new mongoose.Schema({
+    name: { type: String, required: [true, "Comments musts have a name"]},
+    comment: { type: String, required: [true, "Comments must have body"]},
+}, {timestamps: true})
+
+const MessageSchema = new mongoose.Schema({
+    name: { type: String, required: [true, "Messages musts have a name"]},
+    message: { type: String, required: [true, "Messages must have body"]},
+    comments: [CommentSchema]
+}, {timestamps: true})
+
+const Comment = mongoose.model('Comment', CommentSchema);
+const Message = mongoose.model('Message', MessageSchema);
+
+
 // var QuoteSchema = new mongoose.Schema({
 //     name: {type: String, required: true, minlength: 3},
 //     quote: {type: String, required: true, minlength: 10},
@@ -18,32 +34,48 @@ app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-    res.render("index")
+    Message.find({}, function(err, messages) {
+        if (err) {
+            console.log("failure")
+        }
+        else {
+            console.log("success")
+        }
+        res.render("index", {messages:messages})
+    })
 })
 
-// app.post('/quotes', function(req, res) {
-//     var date = new Date();
-//     var quote = new Quote({name: req.body.name, quote: req.body.quote, date: date.getDate()});
-//     // Try to save that new user to the database (this is the method that actually inserts into the db) and run a callback function with an error (if any) from the operation.
-//     quote.save(function(err) {
-//         // if there is an error console.log that something went wrong!
-//         if(err) {
-//             console.log('something went wrong');
-//         } else { // else console.log that we did well and then redirect to the root route
-//             console.log('successfully added a quote!');
-//         }
-//     })
-//     Quote.find({}, function(err, quotes) {
-//         if (err) {
-//             console.log("Error retrieving data from Mongoose");
-//         }
-//         else {
-//             console.log("Successfully got data from Mongoose");
-//         }
-//         res.render("quotes",{quotes:quotes});
-//     })
-// })
+app.post("/message", function(req, res) {
+    var message = new Message({name: req.body.name, message: req.body.message})
+    message.save(function(err) {
+        if (err) {
+            console.log('something went wrong');
+        }
+        else {
+            console.log('successfully added message');
+        }
+    })
+    res.redirect("/")
+})
 
+app.post("/comment", function(req, res) {
+    Comment.create(req.body, function(err, data) {
+        if (err) {
+            console.log("error creating comment");
+        }
+        else {
+            Message.findOneAndUpdate({_id: req.body.id}, {$push: {messages: data}}, function(err, data) {
+                if (err) {
+                    console.log("failure!")
+                }
+                else {
+                    console.log("success!")
+                }
+            })
+        }
+    })
+    res.redirect("/")
+})
 app.listen(8000, function() {
     console.log("listening on port 8000");
 })
